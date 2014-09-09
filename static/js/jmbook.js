73d320850ed9d1873ad086b8ad9ec8c33eaddb0b
JongMan Koo <jongman@gmail.com>
2014-09-10 02:19:47 +0900

    tarjan rewrite draft

--- static/js/jmbook.js
+++ static/js/jmbook.js
@@ -1,4 +1,9 @@
 $(function() {
-	$(".content table").addClass("table-striped");
-	$(".content table").addClass("table");
+	$(".content table").each(function() {
+		var e = $(this);
+		if(!e.hasClass("plain"))
+			e.addClass("table table-striped");
+	});
+	// addClass("table-striped");
+	// $(".content table").addClass("table");
 });
